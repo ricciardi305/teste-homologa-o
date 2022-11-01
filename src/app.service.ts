f7fd545bde47effbf554e6f4cb266ea161e79100
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-11-01 14:30:36 -0300

    connect to newsapi with axios client

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly NEWS_API_KEY = process.env.NEWS_API_KEY;
+  private readonly NEWS_API_URL = process.env.NEWS_API_URL;
+
+  constructor(private readonly httpService: HttpService) {}
+  async getNews() {
+    const url = `${this.NEWS_API_URL}/top-headlines/sources?category=technology`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${this.NEWS_API_KEY}`,
+      },
+    };
+
+    const response = await lastValueFrom(this.httpService.get(url, config));
+
+    console.log(response.data);
+
+    return;
   }
 }
